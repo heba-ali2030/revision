6f3f750db32d12bb780bfa37718849b6e447d590
heba-ali2030 <heba.galaa@gmail.com>
2021-04-24 03:08:16 +0200

    add

---
+++ data_types.py
@@ -0,0 +1,19 @@
+# numbers
+# a= 5
+# b =6.5
+# c = 3+ 5j
+
+# print (type(a))
+# print (type(b))
+# print (type(c))
+# print (c)
+# print(isinstance(c,complex))
+
+# print(isinstance(c,int))
+
+a = [5,10,15,20,25,30,35,40]
+print(a)
+print(a[2])
+print(a[:2])
+a[1]= -10
+print(a)
