b1764e58a4d7f0114f6731d4eafc581ac3abc025
heba-ali2030 <heba.galaa@gmail.com>
2021-04-30 21:46:56 +0200

    add

--- introduction.py
+++ introduction.py
@@ -1,5 +1,18 @@
 
-import constant
+# import constant
 
-print(constant.PI)
-print(constant.GRAVIRTY)
+# print(constant.PI)
+# print(constant.GRAVIRTY)
+
+
+## for loop
+list= ['heba', 'hna', 'hala', 'hager']
+for name in list:
+    print (name[2])
+
+
+numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+sum=0
+for i in numbers:
+    sum= i+ sum
+print(sum)
